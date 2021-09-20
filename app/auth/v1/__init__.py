271052c06b25eb9a580cd2b5c0ba9ce122ff7544
ken <ken.mbira@student.moringaschool.com>
2021-09-20 13:00:10 +0300

    instantiate the resful module

--- app/auth/v1/__init__.py
+++ app/auth/v1/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
+from flask_restful import Api
 
 main = Blueprint('main',__name__,url_prefix='/main')
+api = Api(main,catch_all_404s=True)
 
 from . import user_models,user_views
